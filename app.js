c2ffa7229644a8dce03bb723ed7cdab424a4f60f
Nishanth <nishanthanand2001@gmail.com>
2023-08-06 18:15:54 +0530

    99% finished

--- app.js
+++ app.js
@@ -32,6 +32,14 @@ function PageTransitions() {
       element.classList.add("active");
     }
   });
+
+  //toogle theme//
+  const themeBtn = document.querySelector('.theme-btn');
+  themeBtn.addEventListener('click',() =>{
+    let element = document.body;
+    element.classList.toggle('light-mode')
+
+  })
 }
 
 PageTransitions();
